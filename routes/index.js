2db6aa72ee850dc38eba7e0977ae60c36a25ba44
Prasanna Shiwakoti <Prasanna Shiwakoti>
2017-10-02 11:35:34 -0500

    cors added again

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,12 @@ const heroes = [
       { id: 10, name: 'Tornado' }
     ];
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});    
     
 /* GET home page. */
 router.get('/', function(req, res, next) {
